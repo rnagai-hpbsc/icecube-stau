d0f14beacda8d8d8f7e1fa3ac716f612330e4b38
Ryo Nagai <rnagai@hepburn.s.chiba-u.ac.jp>
2023-03-02 11:31:15 +0900

    added to github repo

---
+++ PropagateStaus.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+from os.path import expandvars
+from icecube import icetray, dataclasses
+from I3Tray import I3Units
+
+import math
+
+def MakePropagator(mediadef=None):
+    """
+    Create a muon propagator service.
+
+    """
+    from icecube import sim_services, PROPOSAL
+    # in PROPOSAL everything can be defined in the configuration file
+    if mediadef is None:
+        mediadef=expandvars('$I3_BUILD/PROPOSAL/resources/config_icesim.json')
+    return PROPOSAL.I3PropagatorServicePROPOSAL(config_file=mediadef)
+
+def DefinePropagators():
+    from icecube import sim_services, phys_services, simclasses, dataclasses
+    from icecube import cmc
+    propagator      = MakePropagator(mediadef='copy_config_icesim.json')
+    cascadePropagator = cmc.I3CascadeMCService(phys_services.I3GSLRandomService(1)) # dummy RNG
+    
+    propagators = sim_services.I3ParticleTypePropagatorServiceMap()
+    for pt in 'MuMinus', 'MuPlus', 'TauMinus', 'TauPlus', 'STauMinus', 'STauPlus':
+        propagators[getattr(dataclasses.I3Particle.ParticleType, pt)] = propagator
+    for pt in 'DeltaE', 'Brems', 'PairProd', 'NuclInt', 'Hadrons', 'EMinus', 'EPlus':
+        propagators[getattr(dataclasses.I3Particle.ParticleType, pt)] = cascadePropagator
+    
+    return propagators
+ 
+@icetray.traysegment
+def PropagateStaus(tray, name,
+    RandomService = None,
+    ):
+
+    from I3Tray import I3Units
+
+    from icecube import icetray, dataclasses, phys_services, sim_services, simclasses
+    from icecube import cmc
+
+    propagator      = MakePropagator(mediadef='copy_config_icesim.json')
+    cascadePropagator = cmc.I3CascadeMCService(phys_services.I3GSLRandomService(1)) # dummy RNG
+
+    print(propagator)
+    stau = dataclasses.I3Particle()
+    stau.type = dataclasses.I3Particle.STauMinus
+    stau.pos = dataclasses.I3Position(0,0,0)
+    stau.dir = dataclasses.I3Direction(0,0)
+    stau.energy = 100 * I3Units.TeV
+    stau.time = 0*I3Units.ns
+    stau.location_type = dataclasses.I3Particle.InIce
+    propagator.register_particletype(stau.type)
+    print(stau)
+    print(propagator.Propagate(stau))
+
+    # set up propagators
+    propagators = sim_services.I3ParticleTypePropagatorServiceMap()
+    for pt in 'MuMinus', 'MuPlus', 'TauMinus', 'TauPlus', 'STauMinus', 'STauPlus':
+        propagators[getattr(dataclasses.I3Particle.ParticleType, pt)] = propagator
+    for pt in 'DeltaE', 'Brems', 'PairProd', 'NuclInt', 'Hadrons', 'EMinus', 'EPlus':
+        propagators[getattr(dataclasses.I3Particle.ParticleType, pt)] = cascadePropagator
+
+    tray.AddModule('I3PropagatorModule', name+'_propagator',
+        PropagatorServices=propagators,
+        RandomService=RandomService,
+        RNGStateName="RNGState",
+        InputMCTreeName="I3MCTree_preStauProp",
+        OutputMCTreeName="I3MCTree")
+
+    # add empty MMCTrackList objects for events that have none
+    def addEmptyMMCTrackList(frame):
+        if "MMCTrackList" not in frame:
+            frame["MMCTrackList"] = simclasses.I3MMCTrackList()
+    tray.AddModule(addEmptyMMCTrackList, name+'_addEmptyMMCTrackList',
+        Streams=[icetray.I3Frame.DAQ])
+
+
+if __name__=="__main__":
+    from optparse import OptionParser
+    from os.path import expandvars
+
+    usage = "usage: %prog [options] inputfile"
+    parser = OptionParser(usage)
+    parser.add_option("-o", "--outfile",default="test_muons_propagated.i3",
+                      dest="OUTFILE", help="Write output to OUTFILE (.i3{.gz} format)")
+    parser.add_option("-s", "--seed",type="int",default=12345,
+                      dest="SEED", help="Initial seed for the random number generator")
+    parser.add_option("-r", "--runnumber", type="int", default=1,
+                      dest="RUNNUMBER", help="The run number for this simulation")
+
+    # parse cmd line args, bail out if anything is not understood
+    (options,args) = parser.parse_args()
+    if len(args) != 0:
+            crap = "Got undefined options:"
+            for a in args:
+                    crap += a
+                    crap += " "
+            parser.error(crap)
+
+    from I3Tray import *
+    import os
+    import sys
+
+    from icecube import icetray, dataclasses, dataio, phys_services
+
+    tray = I3Tray()
+
+    # set up a random number generator
+    randomService = phys_services.I3SPRNGRandomService(
+        seed = options.SEED*2,
+        nstreams = 10000,
+        streamnum = options.RUNNUMBER)
+
+    # re-use the same RNG for modules that need it on the context
+    tray.context['I3RandomService'] = randomService
+
+    tray.AddSegment(PropagateStaus, "PropagateStaus",
+        RandomService = randomService)
+
+    tray.AddModule("I3Writer","writer",
+        Filename = options.OUTFILE)
+
+    tray.Execute(10)
+
+    print(tray)
+
+
---
+++ ppc_example.py
@@ -0,0 +1,87 @@
+import os
+import argparse
+import numpy as np
+from I3Tray import I3Tray, load
+from icecube import icetray, clsim, phys_services, simclasses, photonics_service
+
+
+from PropagateStaus import DefinePropagators
+
+icetray.set_log_level(icetray.I3LogLevel.LOG_TRACE)
+
+
+def main():
+    """ resim for monopod
+    """
+    parser = argparse.ArgumentParser(
+        description='Resimulate benchmark i3s (e.g. under different ice)')
+    parser.add_argument('infiles', nargs='+')
+    parser.add_argument('-o', '--out', default='out.i3.zst',
+                        help='output file')
+    parser.add_argument('-s', '--seed', default=8888, type=int,
+                        help='seed for sprng')
+    parser.add_argument('-r', '--run', default=0, type=int,
+                        help='run for sprng')
+    parser.add_argument('--nframes', type=int, default=None, help='number of frames to process')
+    parser.add_argument('--usecpus', default=False, action='store_true')
+    parser.add_argument('--domos', default=5, type=float)
+    parser.add_argument('--domeff', default=1., type=float)
+    parser.add_argument('--gcd', default=os.path.os.path.expandvars('$I3_DATA/GCD/GeoCalibDetectorStatus_2020.Run134142.Pass2_V0.i3.gz'),
+                        type=str,
+                        help='gcd file')
+    parser.add_argument('--icemodel', choices=('SpiceMie', 'SpiceLea', 'Spice3.2', 'Spice3.2.1',
+                                               'Spicebfr-v1', 'Spicebfr-v2', 'Spicebfr-v2_flat',
+                                               'Spicebfr-v2_tilt'),
+                        default='Spicebfr-v2', help='The ice model to use for photon propagation.')
+    parser.add_argument("--holeice",default='as.h2-50cm', choices=('as.flasher_p1_0.30_p2_0',
+                                                                   'as.flasher_p1_0.30_p2_-1',
+                                                                   'as.flasher_p1_0.30_p2_+1',
+                                                                   'as.flasher_p1_0.30_p2_-2',
+                                                                   'as.flasher_p1_0.30_p2_-3',
+                                                                   'as.h2-50cm',
+                                                                   'as.nominal',
+                                                                   'as.set0_p0=0.0_p1=0.0'), help="Holeice file")
+    parser.add_argument('--nocascadeextension', default=False, action='store_true')
+    parser.add_argument('--notilt', default=False, action='store_true')
+    parser.add_argument('--unweighted', default=False, action='store_true')
+    parser.add_argument('--scale', default=None, type=float)
+    parser.add_argument('--all', default=False, action='store_true')
+    parser.add_argument('--prescale', default=0.01, type=float)
+    parser.add_argument('--history', default=0, type=int)
+    parser.add_argument('--prop',default=None, type=str)
+    parser.add_argument('--usegpus',type=int,default=-1)
+    args = parser.parse_args()
+
+    print("RNG being initiated...")
+    # set up a random number generator
+    randomService = phys_services.I3SPRNGRandomService(
+        seed = args.seed,
+        nstreams = 100000000,
+        streamnum = args.run)
+
+    load('ppc')
+    tray = I3Tray()
+
+    tray.context['I3RandomService'] = randomService
+    tray.Add('I3Reader', Filenamelist=[args.gcd]+args.infiles)
+
+    tray.Add('i3ppc','ppc',
+                     gpu = args.usegpus,
+                     MCTree = "I3MCTree",
+                     infoName = "PPCInfoDict", # create a dict
+                     verbose=True,
+                     photons=True,
+                     pseries=True)
+        
+    tray.Add("Rename", "PPCRename", Keys=["MCPESeriesMap", "I3MCPESeriesMap"])
+
+    tray.Add('I3Writer', filename=args.out,
+             streams=[icetray.I3Frame.DAQ])
+    if args.nframes is None:
+        tray.Execute()
+    else:
+        tray.Execute(args.nframes)
+
+
+if __name__=='__main__':
+    main()
