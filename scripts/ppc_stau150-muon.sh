55c5e6d1b9b4c73948a05ae728e936dd58b1c1c5
Ryo Nagai <rnagai@hepburn.s.chiba-u.ac.jp>
2023-05-18 16:04:53 +0900

    Added generation & ppc scripts

---
+++ scripts/ppc_stau150-muon.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for j in `seq 0 $1`; 
+do 
+    zeroj=`printf '%08d' $j`
+    i=`ls data/$2/Stau_prop/Eslope-*_run${zeroj}.i3`
+    echo $i; 
+    time ./timemeas_generate_ppc_staus_iceinjection.py -o data/$2/Stau_ppc/ --infile $i; 
+done; 
+for j in `seq 0 $1`; 
+do 
+    zeroj=`printf '%08d' $j`
+    i=`ls data/$2/Muon_prop/Eslope-*_run${zeroj}.i3`
+    echo $i; 
+    time ./timemeas_generate_ppc_staus_iceinjection.py -o data/$2/Muon_ppc/ --infile $i; 
+done
